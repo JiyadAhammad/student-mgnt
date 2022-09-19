1b6c8fb27eca053dde24a697e63153b882c01203
JiyadAhammad <jiyadahammad99@gmail.com>
2022-09-19 12:02:06 +0530

    search ready bug fixed

--- lib/view/home/widget/serach.dart
+++ lib/view/home/widget/serach.dart
@@ -43,8 +43,101 @@ class Search extends SearchDelegate {
 
   @override
   Widget buildResults(BuildContext context) {
-    return const Center(
-      child: Text('data'),
+    final searched = studcontroller.list
+        .where(
+          (element) => element.studentName!.toLowerCase().contains(
+                query.toLowerCase(),
+              ),
+        )
+        .toList();
+
+    return Scaffold(
+      backgroundColor: kwhite,
+      body: searched.isEmpty
+          ? ListView(
+              children: [
+                Center(
+                  child: Lottie.asset(
+                    'asset/lottie/emptyfile.json',
+                  ),
+                ),
+                const Center(
+                  child: Text(
+                    "No Data Found!! 😟",
+                    style: TextStyle(
+                      color: kblack,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            )
+          : Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 15,
+                vertical: 15,
+              ),
+              child: ListView.separated(
+                physics: const BouncingScrollPhysics(),
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: kwhite,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        maxRadius: 30,
+                        backgroundImage: FileImage(
+                          File(
+                            searched[index].studentImage!,
+                          ),
+                        ),
+                        child: Container(
+                          height: 200,
+                        ),
+                      ),
+                      onTap: () async {
+                        Get.to(
+                          StudentDetail(
+                            index: index,
+                            data: searched[index],
+                            controllerObj: studcontroller,
+                          ),
+                        );
+                      },
+                      title: Padding(
+                        padding: const EdgeInsets.only(
+                          left: 5.0,
+                          bottom: 3,
+                          top: 3,
+                        ),
+                        child: Text(
+                          searched[index].studentName!,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                            color: kblack,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+                separatorBuilder: (
+                  context,
+                  index,
+                ) {
+                  return const Divider(
+                    height: 20,
+                    thickness: 3,
+                  );
+                },
+                itemCount: searched.length,
+              ),
+            ),
     );
   }
 
@@ -86,67 +179,64 @@ class Search extends SearchDelegate {
                 vertical: 15,
               ),
               child: ListView.separated(
-                  physics: const BouncingScrollPhysics(),
-                  shrinkWrap: true,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      decoration: BoxDecoration(
-                        color: kwhite,
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      child: ListTile(
-                        leading: CircleAvatar(
-                          maxRadius: 30,
-
-                          backgroundImage:
-                              FileImage(File(searched[index].studentImage!)),
-                          // backgroundImage: AssetImage(
-                          //   widget.allAudios[index].metas.image!.path,
-                          // ),
-
-                          //AssetImage(audioList[index].metas.image!.path),
-                          child: Container(
-                            // height: MediaQuery.of(context).size.height * 0.7,
-                            height: 200,
+                physics: const BouncingScrollPhysics(),
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: kwhite,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        maxRadius: 30,
+                        backgroundImage: FileImage(
+                          File(
+                            searched[index].studentImage!,
                           ),
                         ),
-                        onTap: () async {
-                          Get.to(
-                            StudentDetail(
-                              index: index,
-                              data: searched[index],
-                              controllerObj: studcontroller,
-                            ),
-                          );
-                        },
-                        title: Padding(
-                          padding: const EdgeInsets.only(
-                            left: 5.0,
-                            bottom: 3,
-                            top: 3,
+                        child: Container(
+                          height: 200,
+                        ),
+                      ),
+                      onTap: () async {
+                        Get.to(
+                          StudentDetail(
+                            index: index,
+                            data: searched[index],
+                            controllerObj: studcontroller,
                           ),
-                          child: Text(
-                            searched[index].studentName!,
-                            overflow: TextOverflow.ellipsis,
-                            style: const TextStyle(
-                              color: kblack,
-                              fontSize: 18,
-                            ),
+                        );
+                      },
+                      title: Padding(
+                        padding: const EdgeInsets.only(
+                          left: 5.0,
+                          bottom: 3,
+                          top: 3,
+                        ),
+                        child: Text(
+                          searched[index].studentName!,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                            color: kblack,
+                            fontSize: 18,
                           ),
                         ),
                       ),
-                    );
-                  },
-                  separatorBuilder: (
-                    context,
-                    index,
-                  ) {
-                    return const Divider(
-                      height: 20,
-                      thickness: 3,
-                    );
-                  },
-                  itemCount: searched.length),
+                    ),
+                  );
+                },
+                separatorBuilder: (
+                  context,
+                  index,
+                ) {
+                  return const Divider(
+                    height: 20,
+                    thickness: 3,
+                  );
+                },
+                itemCount: searched.length,
+              ),
             ),
     );
   }
